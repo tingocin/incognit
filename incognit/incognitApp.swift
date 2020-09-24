81ca087de006734fa25b8d657e2575f36afcc7e2
vauxhall <zero.griffin@gmail.com>
2020-09-24 12:58:41 +0200

    Initial Commit

---
+++ incognit/incognitApp.swift
@@ -0,0 +1,17 @@
+//
+//  incognitApp.swift
+//  incognit
+//
+//  Created by vaux on 24.09.20.
+//
+
+import SwiftUI
+
+@main
+struct incognitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
