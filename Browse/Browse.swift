a1c8bebdcd2dd40297cfad88bd710deaf25aa91f
vauxhall <zero.griffin@gmail.com>
2020-09-30 11:55:13 +0200

    Update browse

--- Browse/Browse.swift
+++ Browse/Browse.swift
@@ -1,62 +1,43 @@
 import WidgetKit
 import SwiftUI
 
-struct Provider: TimelineProvider {
-    func placeholder(in context: Context) -> SimpleEntry {
-        SimpleEntry(date: Date())
+@main struct Browse: Widget {
+    var body: some WidgetConfiguration {
+        StaticConfiguration(kind: "Browse", provider: Provider()) { entry in
+            VStack {
+                ZStack {
+                    RoundedRectangle(cornerRadius: 20)
+                        .shadow(color: .init(UIColor.systemBackground.withAlphaComponent(0.6)), radius: 4, x: -2, y: -2)
+                        .shadow(color: .init(UIColor.systemBackground.withAlphaComponent(0.6)), radius: 4, x: 2, y: 2)
+                        .foregroundColor(.init(.secondarySystemBackground))
+                    Image(systemName: "magnifyingglass")
+                        .font(.headline)
+                        .foregroundColor(.accentColor)
+                }.frame(height: 40)
+                Spacer()
+            }
+        }
+        .configurationDisplayName("Browse")
+        .description("incognit quick access")
+    }
+}
+
+private struct Provider: TimelineProvider {
+    func placeholder(in: Context) -> Model {
+        .empty
     }
 
-    func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
-        let entry = SimpleEntry(date: Date())
-        completion(entry)
+    func getSnapshot(in: Context, completion: @escaping (Model) -> ()) {
+        completion(.empty)
     }
 
     func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
-        var entries: [SimpleEntry] = []
-
-        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
-        let currentDate = Date()
-        for hourOffset in 0 ..< 5 {
-            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
-            let entry = SimpleEntry(date: entryDate)
-            entries.append(entry)
-        }
-
-        let timeline = Timeline(entries: entries, policy: .never)
-        completion(timeline)
+        completion(Timeline(entries: [.init(pages: 0)], policy: .never))
     }
 }
 
-struct SimpleEntry: TimelineEntry {
-    let date: Date
-}
-
-struct BrowseEntryView : View {
-    var entry: Provider.Entry
-
-    var body: some View {
-        Text(entry.date, style: .time)
-    }
-}
-
-@main
-struct Browse: Widget {
-    let kind: String = "Browse"
-    @State private var text = ""
-
-    var body: some WidgetConfiguration {
-        StaticConfiguration(kind: kind, provider: Provider()) { entry in
-            ZStack {
-                RoundedRectangle(cornerRadius: 20)
-                    .shadow(color: .init(UIColor.systemBackground.withAlphaComponent(0.6)), radius: 4, x: -2, y: -2)
-                    .shadow(color: .init(UIColor.systemBackground.withAlphaComponent(0.6)), radius: 4, x: 2, y: 2)
-                    .foregroundColor(.init(.secondarySystemBackground))
-                Image(systemName: "magnifyingglass")
-                    .font(.headline)
-                    .foregroundColor(.accentColor)
-            }.frame(height: 40)
-        }
-        .configurationDisplayName("My Widget")
-        .description("This is an example widget.")
-    }
+private struct Model: TimelineEntry {
+    static let empty = Self(pages: 0)
+    let pages: Int
+    let date = Date()
 }
