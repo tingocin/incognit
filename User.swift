41159a377a7bcd02497c7921c9d2a9646be74e94
vauxhall <zero.griffin@gmail.com>
2020-10-03 12:37:52 +0200

    Update user

--- User.swift
+++ User.swift
@@ -3,6 +3,9 @@ import Foundation
 struct User: Codable {
     var premium = false
     var rated = false
+    var popups = true
+    var javascript = true
+    var ads = true
     var engine = Engine.ecosia
     let created: Date
     
