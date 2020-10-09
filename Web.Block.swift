35860f608ca098ff25ba6b014f5915fd48fb42ea
vauxhall <zero.griffin@gmail.com>
2020-10-09 16:20:06 +0200

    Update blocker

--- Web.Block.swift
+++ Web.Block.swift
@@ -6,7 +6,16 @@ extension Web {
     {
         "action": {
             "type": "css-display-none",
-            "selector": "div[id='rhs']"
+            "selector": "div[class*='card-ad']"
+        },
+        "trigger": {
+            "url-filter": "https://www.ecosia.org"
+        }
+    },
+    {
+        "action": {
+            "type": "css-display-none",
+            "selector": "div[class*='card-productads']"
         },
         "trigger": {
             "url-filter": "https://www.ecosia.org"
@@ -18,7 +27,16 @@ extension Web {
             "selector": "div[id='taw']"
         },
         "trigger": {
-            "url-filter": "https://www.ecosia.org"
+            "url-filter": "https://www.google.com"
+        }
+    },
+    {
+        "action": {
+            "type": "css-display-none",
+            "selector": "div[id='rhs']"
+        },
+        "trigger": {
+            "url-filter": "https://www.google.com"
         }
     }
 ]
