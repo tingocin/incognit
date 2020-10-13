5ab88781876ad2d896518d9fcc000d22e54374ef
vauxhall <zero.griffin@gmail.com>
2020-10-13 15:12:06 +0200

    Add target

---
+++ watch Extension/incognitApp.swift
@@ -0,0 +1,19 @@
+//
+//  incognitApp.swift
+//  watch Extension
+//
+//  Created by vaux on 13.10.20.
+//
+
+import SwiftUI
+
+@main
+struct incognitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
