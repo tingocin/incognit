1ff67d9a71ae165ee8977bbed8077b3ae4aaa00e
vauxhall <zero.griffin@gmail.com>
2020-10-22 17:11:43 +0200

    Update dark mode

--- Web.Coordinator.swift
+++ Web.Coordinator.swift
@@ -47,7 +47,35 @@ extension Web {
                 
                 configuration.userContentController.addUserScript(.init(source: """
 var color = getComputedStyle(document.querySelector('body'))['background-color'];
-alert("xxxxxxxxxxxxxx:" + getComputedStyle(document.querySelector(':root'))['background-color']);
+
+var s = document.styleSheets, r,
+        i, j, k;
+
+    if(!s) return false; //no style sheets found
+
+// document.styleSheets[0].cssRules[3].media.mediaText.indexOf('dark')
+
+    // walk throuth css sheets
+    for(i=0; i<s.length; i++) {
+        // get all rules
+        r = s[i].cssRules;
+        if(!r) continue;
+
+        for(j=0; j<r.length; j++) {
+            //If there's a rule for media query
+            if(r[j] instanceof CSSMediaRule &&
+                    r[j].media.mediaText == "only screen and (min-width: 600px)") {
+                for(k=0; k<r[j].cssRules.length; k++) {
+                    // remove all rules of it
+                    r[j].deleteRule(r[j].cssRules[k]);
+                }
+                return true;
+            }
+        }
+    }
+
+
+alert("xxxxxxxxxxxxxx:" + getComputedStyle(document.querySelector(':root'))['color-scheme']);
 if (color == "rgb(255, 255, 255)") {
 
     var style = document.createElement('style');
